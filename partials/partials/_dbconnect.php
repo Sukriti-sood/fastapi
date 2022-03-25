49625ed8da332dc985c551305cdd2f0c0e9fecf5
Sukriti Sood <55010599+Sukriti-sood@users.noreply.github.com>
2022-03-26 01:07:53 +0530

    Delete partials directory

--- partials/partials/_dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Script to connect to the database
-$servername = "";
-$username = "";
-$password = "";
-$database = "";
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
