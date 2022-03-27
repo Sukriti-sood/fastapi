f5deed1b5bd24bf8c30b44fabee07fedbb0dbb80
Sukriti-sood <soodsukriti1111@gmail.com>
2022-03-27 05:17:16 +0000

    Syncing from anax

---
+++ docs/anax/_dbconnect.php
@@ -0,0 +1,8 @@
+<?php
+// Script to connect to the database
+$servername = "";
+$username = "";
+$password = "";
+$database = "";
+$conn = mysqli_connect($servername, $username, $password, $database);
+?>
